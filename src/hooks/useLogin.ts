ed53a276f0d2f2d3ca3eb81977b54a11616c8c31
IamPossible007 <113297734+IamPossible007@users.noreply.github.com>
2024-09-15 22:10:08 +0530

    jwt2

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -19,6 +19,7 @@ const useLogin = () => {
       headers: {
         "Content-Type": "application/json",
       },
+      credentials: "include",
       body: JSON.stringify(request),
     });
     if (!res.ok) {
