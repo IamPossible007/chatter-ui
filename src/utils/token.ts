36d62f14388bd4fd3f7489cc6248d68e0196557d
IamPossible007 <113297734+IamPossible007@users.noreply.github.com>
2024-09-15 21:55:46 +0530

    jwt1

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,7 +1,11 @@
-export const setToken = (token: string) => {};
+
+
+const TOKEN = "token";
+export const setToken = (token: string) => localStorage.setItem(TOKEN, token);
 
 export const getToken = () => {
-  return "";
+    const token = localStorage.getItem(TOKEN);
+    return token ? `Bearer ${token}` : "";
 };
 
 export const clearToken = () => localStorage.clear();
