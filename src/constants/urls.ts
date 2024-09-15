2b74bc0a47675a8a7bec7a0bc17ddd8c6a620717
IamPossible007 <113297734+IamPossible007@users.noreply.github.com>
2024-09-15 13:31:54 +0530

    env commit

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,4 +1,4 @@
-const API_URL = `http://localhost:3000`;
-const WS_URL = `localhost:3001`;
+const API_URL = process.env.REACT_APP_API_URL
+const WS_URL = process.env.REACT_APP_WS_URL;
 
 export { API_URL,WS_URL };
